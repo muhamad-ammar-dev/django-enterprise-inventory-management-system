6e02bc642eface7c20fbb0fc1340e88b343b728b
Muhamad Ammar <muhamedammar0900@gmail.com>
2026-07-09 10:50:59 -0700

    Delete home directory

--- home/views.py
+++
@@ -1,7 +0,0 @@
-from django.shortcuts import render, redirect
-
-# Create your views here.
-def home(request):
-    if not request.user.is_authenticated:
-        return redirect('login')
-    return render(request, 'home.html', {})
