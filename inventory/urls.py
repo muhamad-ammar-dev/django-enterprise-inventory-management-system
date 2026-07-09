88ae98492bf1c6fb8056a6426824b4bab874386f
Muhamad Ammar <muhamedammar0900@gmail.com>
2026-07-09 10:51:09 -0700

    Delete inventory directory

--- inventory/urls.py
+++
@@ -1,9 +0,0 @@
-from django.urls import path
-from .views import *
-
-urlpatterns = [
-    path('stock_list/', StockListView.as_view(), name='stock_list'),
-    path('create_stock/', CreateStock.as_view(), name='create_stock'),
-    path('update_stock/<str:pk>/', UpdateStock.as_view(), name='update_stock'),
-    path('delete_stock/<str:pk>/', DeleteStock.as_view(), name='delete_stock'),
-]
