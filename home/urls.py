b45240ea73aed773b0213ff7b79358bf32ff5094
Muhamad Ammar <muhamedammar0900@gmail.com>
2026-07-16 16:02:40 -0700

    Delete home directory

--- home/urls.py
+++
@@ -1,8 +0,0 @@
-from django.urls import path
-from django.contrib.auth.decorators import login_required
-from . import views
-
-urlpatterns = [
-    path('', login_required(views.home), name='home'),
-    path('about/', views.AboutView.as_view(), name='about'),
-]
